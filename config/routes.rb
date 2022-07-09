5a930f23e06ba4c84cfaf6f7becc7e65437625eb
lukasz.kowalik <lkowalik04@gmail.com>
2022-07-09 17:16:05 +0200

    Adding ficzers

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "main#index"
-  get "about-us", to: 'about#index', as: :about
+
+  get "about", to: "about#index", as: :about
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
+  get "sign-up", to: "registrations#new"
+  post "sign-up", to: "registrations#create"
+
+  get "sign-in", to: "sessions#new"
+  post "sign-in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 end
